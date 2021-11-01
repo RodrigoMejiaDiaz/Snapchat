fe92bc9cd6d622e8aaa298d94a87b56712758369
Rodrigo Mejia <rodrigoemdiaz@hotmail.com>
2021-11-01 13:22:53 -0700

    Initial commit 2

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by RTodrigo Mejia on 11/1/21.
+//  Copyright © 2021 Rodrigo Mejia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
